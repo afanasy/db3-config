36720137134e634be436a50c1f1b83f69c7df7f4
Raabb Ajam <raabbajam@gmail.com>
2015-11-09 13:56:59 +0700

    Added push method

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var
   db3 = require('db3'),
+  qs = require('querystring'),
   _ = require('underscore')
 
 
 function Db3Config (opts) {
   if (this.constructor !== Db3Config)
     return new Db3Config(opts)
-  if (!opts.database)
-    throw (new Error('Please specify database name'))
   opts.host = opts.host || 'localhost'
   opts.user = opts.user || 'root'
   opts.password = opts.password || ''
@@ -15,12 +14,8 @@ function Db3Config (opts) {
   this.db = db3.connect(opts)
 }
 
-Db3Config.prototype.pull = pull
-Db3Config.prototype.push = push
-
-function pull (done) {
-  var
-    db3Config = this
+Db3Config.prototype.pull = function pull (done) {
+  var db3Config = this
 
   getTables(db3Config, function (err, tables) {
     if (err) return done(err)
@@ -29,6 +24,8 @@ function pull (done) {
       count = 0,
       target = tables.length
 
+    if (!tables.length)
+      return done(null, formatOutput([]))
     tables.forEach(function (table) {
       describeTable(db3Config, table, function (err, fields) {
         if (err) return done(err)
@@ -42,8 +39,61 @@ function pull (done) {
   })
 }
 
-function push (config, done) {
-  done()
+Db3Config.prototype.push = function push (config, done) {
+  var
+    db3Config = this,
+    db = { table: { } },
+    queries = []
+
+  db3Config.pull(function (err, localDb) {
+    _.each(config.table, function (config, table) {
+      if (_.has(config, 'field')) {
+        db.table[table] = _.pick(config, ['field', 'key'])
+      }
+    })
+    _.each(db.table, function (table, id) {
+
+      // if not present in old db, create
+      if (!localDb.table[id]) {
+        db.table[id].id = id
+        queries.push(create(table))
+
+      // table presents in old db
+      } else {
+
+        // check for new field in new config
+        _.each(db.table[id].field, function (field, fieldId) {
+
+          // if new field not present in local db, add
+          if (!localDb.table[id].field[fieldId]) {
+            var fieldObj = {}
+            fieldObj[fieldId] = field
+            queries.push(add(id, fieldObj))
+          }
+        })
+
+        // check for old local db
+        _.each(localDb.table[id].field, function (field, fieldId) {
+
+          // if old field not present in new config, drop
+          if (!db.table[id].field[fieldId]) {
+            queries.push(drop(id, fieldId))
+          }
+        })
+      }
+    })
+    var
+      count = 0,
+      target = queries.length
+    queries.forEach(function (query) {
+      db3Config.db.query(query, function (err) {
+        if (err) return done(err)
+        if (++count === target) {
+          return done()
+        }
+      })
+    })
+  })
 }
 
 module.exports = Db3Config
@@ -74,7 +124,7 @@ function formatField (field) {
   var output = { key: field.Field }, value
   if (field.Key === 'PRI') {
     value = true
-  } else if (field.Null === 'NO' && field.Default === null && field.Key === '') {
+  } else if (field.Default === null && field.Key === '') {
     value = field.Type
   } else {
     value = {
@@ -109,5 +159,100 @@ function formatOutput (tables) {
     }
   })
   return output
+}
 
+function createQuery(fields, keys) {
+  keys = keys || {}
+  var
+    queries = [],
+    queryKeys = []
+
+  _.each(fields, function (attr, field) {
+
+    // create array, later will be joined
+    var query = ['`' + field + '`']
+
+    // if true, field is primary key
+    if (attr === true) {
+      query.push('bigint(20) NOT NULL AUTO_INCREMENT')
+      queryKeys.push('PRIMARY KEY (`' + field + '`)')
+
+    // if string, field is data type
+    } else if (_.isString(attr)) {
+      query.push(attr)
+
+    // if object, create based on key / property
+    } else if (_.isObject(attr)) {
+      var dataType = attr.dataType || 'text'
+      query.push(dataType)
+
+      // unless specified, always put not null
+      if (attr.notNull === false) query.push('NOT NULL')
+      if (_.has(attr, 'default')) query.push('DEFAULT ' + qs.escape(attr.default))
+    }
+
+    // if field has key option
+    if (_.has(keys, field)) {
+      var attr = keys[field]
+
+      // if true, field is indexed
+      if (attr === true) {
+        queryKeys.push('KEY `' + field + '` (`' + field + '`)')
+
+      // if object, check if it is unique index
+      } else if (_.isObject(attr)) {
+        var
+          keyFields = [],
+          keyType = 'KEY'
+
+        // if no field option, use field name
+        if (!_.has(attr, 'field')) {
+          keyFields = [field]
+
+        // if there is field option, find field name
+        } else {
+
+          // if true, use field name
+          if (attr.field === true) {
+            keyFields.push(field)
+
+          // if string, use it as field name
+          } else if (_.isString(attr.field)) {
+            keyFields.push(attr.field)
+
+          // if array, use all
+          } else if (_.isArray(attr.field)) {
+            keyFields = keyFields.concat(attr.field)
+          }
+        }
+
+        if (attr.unique)
+          keyType = 'UNIQUE KEY'
+        queryKeys.push(keyType + ' `' + field + '` (`' + keyFields.join('`, `') + '`)')
+
+      }
+    }
+    queries.push(query.join(' '))
+  })
+  queries = queries.concat(queryKeys)
+  return queries
+}
+
+function create(table) {
+  var query = 'CREATE table `' + table.id + '` (' +
+    createQuery(table.field, table.key).join(', ') +
+    ') ENGINE=myisam'
+  return query
+}
+
+function add(table, fields) {
+  var query = 'ALTER TABLE `' + table + '` ADD ' +
+    createQuery(fields).join(', ADD ')
+  return query
+}
+
+function drop(table, fields) {
+  fields = _.isArray(fields) ? fields : [fields]
+  var query = 'ALTER TABLE `' + table + '` DROP `' + fields.join('`, DROP `') + '`'
+  return query
 }
--- test.js
+++ test.js
@@ -9,29 +9,46 @@ var
     database: process.env.DB_NAME
   },
   config = { "table": { "user": { "field": { "id": true, "username": "text", "password": "varchar(32)", "subscribed": { "dataType": "tinyint(4)", "default": "0" } }, "key": { "subscribed": true } } } },
+  alteredConfig = { "table": { "user": { "field": { "id": true, "username": "text", "password": "varchar(32)", "new": "text", "subscribed": { "dataType": "tinyint(4)", "default": "0" } }, "key": { "subscribed": true } } } },
   expected = {
-    pull: { "table": { "user": { "field": { "id": true, "username": "text", "password": "varchar(32)", "subscribed": { "dataType": "tinyint(4)", "default": "0" } }, "key": { "subscribed": true } } } }
+    pull: { "table": { "user": { "field": { "id": true, "username": "text", "password": "varchar(32)", "new": "text", "subscribed": { "dataType": "tinyint(4)", "default": "0" } }, "key": { "subscribed": true } } } },
   }
 
 describe('Db3Config module', function () {
   this.timeout(10 * 1000)
+  var db3Config
+  before(function(done) {
+    db3Config = new Db3Config(opts)
+    db3Config.db.dropTable('user', function (err) {
+      done()
+    })
+  });
   describe('basic initialization', function () {
     it('always called using new', function () {
-      var db3Config = Db3Config(opts)
       expect(db3Config.constructor).to.be(Db3Config)
     })
     it('throws error when required parameters are not provided', function () {
       expect(Db3Config).withArgs().to.throwError()
     })
   })
+  describe('push method', function () {
+    it('alter db from JSON', function (done) {
+      db3Config.push(alteredConfig, function (err, data) {
+        if (err) return done(err)
+        db3Config.pull(function (err, newLocalDb) {
+          if (err) return done(err)
+          expect(newLocalDb).to.eql(alteredConfig)
+          done()
+        })
+      })
+    })
+  })
   describe('pull method', function () {
     it('reads data from db into JSON', function (done) {
-      var db3Config = Db3Config(opts)
       db3Config.pull(function (err, data) {
         if (err) {
           return done(err)
         }
-        // console.log(JSON.stringify(data, null, 2))
         expect(data.table.user.field.id).to.be.ok
         expect(data).to.eql(expected.pull)
         done()
